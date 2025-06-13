957b5151989464676bb6c59bd5cf379f1057d7e7
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2025-06-14 00:25:53 +0100

    📄 Added copyright notice in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/* Copyright (C) 2025 anonymous
+This file is a port of https://github.com/kmeps4/PSFree/blob/main/lapse.mjs
+which is part of the PSFree project.
+
+PSFree is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+PSFree is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #include <mast1c0re.hpp>
 #include "../helpers/helpers.cpp"
 #include "../helpers/ruby_chan.cpp"
